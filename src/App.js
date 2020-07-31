2e6950568c3d15a816a52fb6dc0131bacdcae9a9
Jose Sustaita <josesustaita70@gmail.com>
2020-07-30 21:46:24 -0400

    Able to post classes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import './App.scss';
-import SignUp from './components/SignUp'
-import Homepage from './components/Homepage'
-import Login from './components/Login'
-import AboutUs from './components/AboutUs'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React from "react";
+import "./App.scss";
+import SignUp from "./components/SignUp";
+import Homepage from "./components/Homepage";
+import Login from "./components/Login";
+import AboutUs from "./components/AboutUs";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import {connect} from "react-redux";
-import './App.css';
-import SignUp from './components/SignUp'
-import Homepage from './components/Homepage'
-import Login from './components/Login'
 import PrivateRoute from "./components/PrivateRoute";
 import ClientDashboard from "./components/ClientDashboard";
 import InstructorDashboard from "./components/InstructorDashboard";
 
 function App() {
-
   return (
-    <div className = "App">
-      <Route exact path="/" component= {Homepage}/>
-      <Route path= "/signup" component= {SignUp}/>
-      <Route path= "/login" component= {Login}/>
-      <Route path= "/aboutus" component= {AboutUs}/>
+    <div className="App">
+      <Route exact path="/" component={Homepage} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/login" component={Login} />
+      <Route path="/aboutus" component={AboutUs} />
 
       <Switch>
-        <PrivateRoute path="/client" component={ClientDashboard} />
-        <PrivateRoute path="/instructor" component={InstructorDashboard}/>
+        <Route path="/client" component={ClientDashboard} />
+        <Route path="/instructor" component={InstructorDashboard} />
       </Switch>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/AddClass.js
+++ src/components/AddClass.js
@@ -6,7 +6,15 @@ import { axiosWithAuth } from "../utils/axiosWithAuth.js";
 const AddClass = (props) => {
   const [name, setName] = useState("");
   const [type, setType] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [intensity, setIntensity] = useState("");
   const [location, setLocation] = useState("");
+  const [
+    numberOfRegisteredAttendees,
+    setNumberOfRegisteredAttendees,
+  ] = useState(0);
+  const [maxClassSize, setMaxClassSize] = useState(0);
 
   // handle name changes
   const handleName = (e) => {
@@ -16,6 +24,26 @@ const AddClass = (props) => {
   // handle name changes
   const handleType = (e) => {
     setType(e.target.value);
+  };
+  // handle email changes
+  const handleTime = (e) => {
+    setStartTime(e.target.value);
+  };
+  // handle email changes
+  const handleDuration = (e) => {
+    setDuration(e.target.value);
+  };
+  // handle email changes
+  const handleIntensity = (e) => {
+    setIntensity(e.target.value);
+  };
+  // handle email changes
+  const handleRegistered = (e) => {
+    setNumberOfRegisteredAttendees(e.target.value);
+  };
+  // handle email changes
+  const handleMax = (e) => {
+    setMaxClassSize(e.target.value);
   };
 
   // handle email changes
@@ -27,22 +55,31 @@ const AddClass = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axiosWithAuth()
-      .post("https://anywhere-fitnesssite.herokuapp.com/classes/", {
+      .post("https://anywhere-fitnesssite.herokuapp.com/classes", {
         name: name,
         type: type,
         location: location,
+        startTime: startTime,
+        duration: duration,
+        intensity: intensity,
+        numberOfRegisteredAttendees: numberOfRegisteredAttendees,
+        maxClassSize: maxClassSize,
       })
       .then(console.log("good"))
       .catch((err) => console.log(err));
 
     setName("");
     setType("");
+    setStartTime("");
+    setDuration("");
+    setIntensity("");
     setLocation("");
+    setNumberOfRegisteredAttendees("");
+    setMaxClassSize("");
   };
 
   return (
     <div className="add-workout">
-
       <form onSubmit={handleSubmit} className={`add-container`}>
         <input
           type="text"
@@ -64,10 +101,51 @@ const AddClass = (props) => {
 
         <input
           type="text"
+          name="startTime"
+          value={startTime}
+          onChange={handleTime}
+          placeholder="Start Time"
+          className={`add-input`}
+        />
+        <input
+          type="number"
+          name="duration"
+          value={duration}
+          onChange={handleDuration}
+          placeholder="Duration"
+          className={`add-input`}
+        />
+        <input
+          type="text"
+          name="intensity"
+          value={intensity}
+          onChange={handleIntensity}
+          placeholder="Intensity"
+          className={`add-input`}
+        />
+
+        <input
+          type="text"
           name="location"
           value={location}
           onChange={handleLocation}
           placeholder="Location"
+          className={`add-input`}
+        />
+        <input
+          type="number"
+          name="numberOfRegisteredAttendees"
+          value={numberOfRegisteredAttendees}
+          onChange={handleRegistered}
+          placeholder="Registed Attendees"
+          className={`add-input`}
+        />
+        <input
+          type="number"
+          name="maxClassSize"
+          value={maxClassSize}
+          onChange={handleMax}
+          placeholder="Max Class Size"
           className={`add-input`}
         />
         <button className="add-button">Add Class</button>
