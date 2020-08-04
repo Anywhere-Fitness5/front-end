fcb5ec474b817eafe791d638ac7e794cc23e8bba
Tashby <tashby2714@gmail.com>
2020-08-04 19:05:24 -0400

    routing error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,10 +10,10 @@ const NavBar = () => {
                 <h1>Anywhere Fitness</h1>
             </div>
             <div className="links">
-                <Link path to="/">Home</Link>
-                <Link path to="/signup">Sign Up</Link>
-                <Link path to="/login">Login/Logout</Link>
-                <Link path to="/aboutus">About Us</Link>
+                <Link to="/">Home</Link>
+                <Link to="/signup">Sign Up</Link>
+                <Link to="/login">Login/Logout</Link>
+                <Link to="/aboutus">About Us</Link>
             </div>
 
         </div>
